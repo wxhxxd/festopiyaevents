0f97ee1426bb63cc11e3cba12515f15c61a1934a
Your Name <your-email@gmail.com>
2026-06-27 00:18:47 +0530

    style: remove spaces between logo characters and center SVG text

--- src/components/FestopiyaBranding.tsx
+++ src/components/FestopiyaBranding.tsx
@@ -12,15 +12,16 @@ export default function FestopiyaBranding({ className = "", isLanding = false }:
         className={`select-none overflow-visible ${className}`}
         style={{
           height: "1.1em",
-          width: "5.5em",
+          width: "6.2em",
           display: "inline-block",
           verticalAlign: "middle",
         }}
         viewBox="0 0 180 32"
       >
         <text
-          x="0"
+          x="50%"
           y="25"
+          textAnchor="middle"
           className="select-none"
           style={{
             fontFamily: "'Outfit', sans-serif",
@@ -29,9 +30,7 @@ export default function FestopiyaBranding({ className = "", isLanding = false }:
             letterSpacing: "-0.03em",
           }}
         >
-          <tspan className="dash-f">F</tspan>
-          <tspan className="dash-e">e</tspan>
-          <tspan className="dash-stopiya">stopiya</tspan>
+          <tspan className="dash-f">F</tspan><tspan className="dash-e">e</tspan><tspan className="dash-stopiya">stopiya</tspan>
         </text>
       </svg>
     </span>
